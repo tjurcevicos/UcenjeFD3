ba377d37f0b71d6a1c4dd98fea8b6687b236e967
Tomislav Jurčević <t.jurcevic.os@gmail.com>
2026-07-13 18:55:47 +0200

    Uvod u JS

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,24 @@
+// Ovo je komentar
+//alert('Hello World 1') // ; se može pisati i ne mora
+
+/*
+
+Ovo je komentar od više linija
+
+*/
+
+
+document.getElementById('ispis').innerText='Hello world 6'
+document.getElementById('ispis').style.color='red'
+
+document.getElementById('ge').innerHTML =
+'<a href="#">Ovo je poveznica</a>'
+
+// Backtics 
+console.log(`
+    
+    Ovaj tekst mogu 
+    pisati kroz 
+    više redova
+   
+    `)

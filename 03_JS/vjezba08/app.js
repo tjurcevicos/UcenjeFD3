77973fb25f2faf8be9f5a800a1250a4993e63200
Tomislav Jurčević <t.jurcevic.os@gmail.com>
2026-07-27 12:12:11 +0200

    Vježba 08

---
+++ 03_JS/vjezba08/app.js
@@ -0,0 +1,159 @@
+
+
+/* =================================================
+   ZADATAK 1 — Prvi i zadnji element
+   - niz se gradi od A i B (plus dva fiksna elementa radi smisla)
+   Ulaz primjer: [A, 20, 30, B]
+   Izlaz: Prvi: A, Zadnji: B
+================================================= */
+
+
+document.getElementById('zad01').addEventListener('click', () => {
+   console.log("ZADATAK 1 — Prvi i zadnji element")
+
+   const a = document.getElementById('inA').value
+   const b = document.getElementById('inB').value
+
+   // ne mogu mijenjati konstantu
+   // if(!a){
+   //    a = 'Prazno'
+   // }
+
+   // if(!b){
+   //    b = 'Prazno'
+   // }
+
+   const av = !a ? 'Prazno' : a
+   const bv = !b ? 'Prazno' : b
+
+   const niz = [av, 20, 30, bv]
+
+   console.log(`Prvi: ${niz[0]}, Zadnji: ${niz[niz.length-1]}`)
+
+
+})
+
+
+
+/* =================================================
+   ZADATAK 2 — Siguran pristup elementu
+   U danom nizu [1,2,3,4,5,6,7]
+   - Ako je A broj postavi vrijednost B na taj index
+   - Ako je A string u niz dodaj novi element i postavi B na taj index
+================================================= */
+console.log("ZADATAK 2 — Siguran pristup elementu")
+
+document.getElementById('zad02').addEventListener('click', () => {
+
+   const a = document.getElementById('inA').value;
+   const b = document.getElementById('inB').value;
+
+   const niz = [1,2,3,4,5,6,7];
+
+   const brojaA = Number(a);
+
+   if (!brojaA){
+      niz.lenght = niz.lenght + 1;
+      niz[niz.lenght - 1] =b;
+   }else{
+      niz[brojaA] = b;
+   }
+   console.log(niz);
+   console.log("Zadatak 2 - Siguran pristup elementu")
+
+})
+
+
+
+
+/* =================================================
+   ZADATAK 3 — Zamjena prva dva elementa
+   Unesi podatke A i B u niz [A, B, 3]
+   - ispiši niz
+   - zamijeni prva dva elemente
+   - ispiši niz
+================================================= */
+console.log("ZADATAK 3 — Zamjena prva dva elementa")
+
+document.getElementById('zad03').addEventListener('click', () => {
+
+   const a = document.getElementById('inA').value;
+   const b = document.getElementById('inB').value;
+
+   const niz = [a, b, 3]
+
+   console.log(niz)
+   const manta = niz[0]
+   niz[0] = niz[1]
+   niz[1] = manta
+
+   console.log(niz)
+   
+
+})
+
+
+
+
+/* =================================================
+   ZADATAK 4 — Dodavanje elementa prema duljini
+   - niz: [A, B]
+   - ako je A string i duljina(A) < 3 → dodaj "X" na kraj
+     inače → dodaj "Y" na početak
+================================================= */
+console.log("ZADATAK 4 — Dodavanje elementa prema duljini")
+
+
+
+/* =================================================
+   ZADATAK 5 — Provjera tipa prvog elementa
+   - ako je A broj 7 na prvi elementi niza podatvi true, inače 0
+   - ako je B tekst Osijek na drugi element postavi B, inače 2,5
+   - koristeći elemente niza ispiši kojeg su tipa
+================================================= */
+console.log("ZADATAK 5 — Provjera tipa elemenata")
+
+
+/* =================================================
+   ZADATAK 6 — Ukloni zadnji element 
+  Vrijednosti A i B se unose u niz [A, B, 'KRAJ']
+================================================= */
+console.log("ZADATAK 6 — Ukloni zadnji element")
+
+
+/* =================================================
+   ZADATAK 7 — Spoji dva niza
+   - niz1: [A, 2]
+   - niz2: [B, 4]
+   - spajanje spreadom
+================================================= */
+console.log("ZADATAK 7 — Spoji dva niza")
+
+
+/* =================================================
+   ZADATAK 8 — Provjera rupe u nizu
+   - napravimo niz sa "rupom" na indeksu 1:
+     niz[0]=A, niz[2]=B
+   - provjerimo indeks 1
+================================================= */
+console.log("ZADATAK 8 — Provjera rupe u nizu")
+
+
+/* =================================================
+   ZADATAK 9 — Razdvajanje prvog i ostatka
+   - niz: [A, B, 7, 6]
+   - destructuring [prvi, ...ostatak]
+================================================= */
+console.log("ZADATAK 9 — Razdvajanje prvog i ostatka")
+
+
+/* =================================================
+   ZADATAK 10 — prema duljini niza
+   - duljinu određujemo prema A i B:
+     Ako je A prazan string ili B prazan string → niz = []
+     Inače ako A === B → niz = [A]
+     Inače ako je B broj i B === 2 → niz = [A, B]
+     Inače → niz = [A, B, "x"]
+================================================= */
+console.log("ZADATAK 10 — prema duljini niza")
+
